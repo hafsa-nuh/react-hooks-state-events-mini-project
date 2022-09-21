cd661b597163040c91f1c20a62a9d8bad0925fca
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-09-21 11:14:38 +0300

    not done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, deleteTask }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      <Task
+        key={tasks.text}
+        text={tasks.text}
+        category={tasks.category}
+        deleteTask={deleteTask}
+      />
     </div>
   );
 }
